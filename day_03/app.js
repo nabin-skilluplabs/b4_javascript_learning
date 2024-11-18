868a88ff14f0c00c7664843935923a1ebfd5cb95
Nabin Singh <nabin@skilluplabs.com.au>
2024-11-18 19:55:41 +1100

    Day 3

---
+++ day_03/app.js
@@ -0,0 +1,3 @@
+let normalName              =                     "Alex";
+let javaScriptName = normalName + "Script";
+console.log('Your JavaScript Name is                        ' + javaScriptName);
