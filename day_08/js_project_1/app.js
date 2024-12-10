30d3e7c553d2e97c2533f025bc4c13947ee5d97a
Nabin Singh <nabin@skilluplabs.com.au>
2024-12-10 18:53:58 +1100

    Added event listener

---
+++ day_08/js_project_1/app.js
@@ -0,0 +1,9 @@
+const plusBtn = document.getElementById('plus_btn');
+plusBtn.addEventListener('click', function () {
+  console.log('+ Clicked');
+});
+
+const minusBtn = document.getElementById('minus_btn');
+minusBtn.addEventListener('click', function () {
+  console.log('- Clicked');
+});
